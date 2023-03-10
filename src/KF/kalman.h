842e98b63c6ff019c5863f51c3f2c6eacf8316ca
davi1055 <1224802565@qq.com>
2023-03-11 01:46:36 +0800

    第二次更改

--- src/KF/kalman.h
+++ src/KF/kalman.h
@@ -1,25 +1,25 @@
-// #ifndef KALMAN
-// #define	KALMAN
+#ifndef KALMAN
+#define	KALMAN
 
-// #include <iostream>
-// #include <math.h>
-// #include <fstream>
-// #include <array>
-// #include <eigen3/Eigen/Dense>
-// #include <vector>
-// #include <pybind11/pybind11.h>
-// #include <opencv4/opencv2/core.hpp>
-// #include <opencv2/opencv.hpp>
-// #include "ArmorCenter.h"
-// #include "AngleSolve.h"
+#include <iostream>
+#include <math.h>
+#include <fstream>
+#include <array>
+#include <eigen3/Eigen/Dense>
+#include <vector>
+#include <pybind11/pybind11.h>
+#include <opencv4/opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include "ArmorCenter.h"
+#include "AngleSolve.h"
 
-// class Kalman
-// {
-// public:
+class Kalman
+{
+public:
 	
 
-// 	MatrixXf& predict();
-// }
+	MatrixXf& predict();
+}
 
 
 
@@ -34,4 +34,4 @@
 
 
 
-// #endif KALMAN
+#endif KALMAN
